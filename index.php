d24402bdcdbeed692e791a6a08a6975daad198d1
CrispoDaniel <crispodaniel97@gmail.com>
2020-09-11 11:38:10 +0700

    Update index.php
    Editing file

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/testimonials/logo.png" rel="icon">
-  <link href="assets/img/testimonials/logo.png" rel="cd">
+  <link href="logo.png" rel="icon">
+  <link href="logo.png" rel="cd">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="bootstrap.min.css" rel="stylesheet">
+  <link href="icofont.min.css" rel="stylesheet">
+  <link href="boxicons.min.css" rel="stylesheet">
+  <link href="venobox.css" rel="stylesheet">
+  <link href="owl.carousel.min.css" rel="stylesheet">
+  <link href="aos.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
@@ -45,7 +45,7 @@
     <div class="d-flex flex-column">
 
       <div class="profile">
-        <img src="assets/img/crispo.jpg" alt="" class="img-fluid rounded-circle">
+        <img src="crispo.jpg" alt="" class="img-fluid rounded-circle">
         <h1 class="text-light"><a href="index.html">Crispo Daniel</a></h1>
         <div class="social-links mt-3 text-center">
           <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
@@ -537,20 +537,20 @@
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/typed.js/typed.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="jquery.min.js"></script>
+  <script src="bootstrap.bundle.min.js"></script>
+  <script src="jquery.easing.min.js"></script>
+  <script src="validate.js"></script>
+  <script src="jquery.waypoints.min.js"></script>
+  <script src="counterup.min.js"></script>
+  <script src="isotope.pkgd.min.js"></script>
+  <script src="venobox.min.js"></script>
+  <script src="owl.carousel.min.js"></script>
+  <script src="typed.min.js"></script>
+  <script src="aos.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="main.js"></script>
 
 </body>
 
